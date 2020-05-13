58e0e22df9c3a1fed8da0895bd0bce149723ce5e
shimital <shimital269@gmail.com>
2020-05-13 14:33:16 +0300

    add validation tests

--- projects/ring-chart/src/lib/ring-chart.service.spec.ts
+++ projects/ring-chart/src/lib/ring-chart.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { RingChartService } from './ring-chart.service';
 
 describe('RingChartService', () => {
@@ -104,5 +103,64 @@ describe('RingChartService', () => {
     expect(partition.leftSections[0].color).toEqual('red');
     expect(partition.leftSections[0].index).toEqual(0);
     expect(partition.leftSections[0].rotation).toEqual(180);
+  });
+
+  it('should make sure sections array validation works properly', () => {
+    const sections = 5;
+
+    expect(() => service.validateSections(sections as any))
+      .toThrowError('sections must be of type array');
+  });
+
+  it('should make sure validations for section percantages works properly', () => {
+    const sections1 = [
+      {
+        percentage: 1.2,
+        color: 'red'
+      }
+    ];
+
+    expect(() => service.validateSections(sections1 as any))
+      .toThrowError('section percentage must be a number and not bigger than 1');
+
+    const sections2 = [
+      {
+        percentage: 'blabla',
+        color: 'red'
+      }
+    ];
+
+    expect(() => service.validateSections(sections2 as any))
+      .toThrowError('section percentage must be a number and not bigger than 1');
+  });
+
+  it('should make sure sections percentages are exactly one', () => {
+    const sections1 = [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.6,
+        color: 'green'
+      }
+    ];
+
+    expect(() => service.validateSections(sections1 as any))
+      .toThrowError('sections percentage sum must be equal to 1');
+
+    const sections2 = [
+      {
+        percentage: 0.3,
+        color: 'red'
+      },
+      {
+        percentage: 0.4,
+        color: 'green'
+      }
+    ];
+
+    expect(() => service.validateSections(sections2 as any))
+      .toThrowError('sections percentage sum must be equal to 1');
   });
 });
