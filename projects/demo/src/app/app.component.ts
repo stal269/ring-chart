ab1174bd2a3a2c6fe7eca53a16f18911d6fafbd3
shimital <shimital269@gmail.com>
2020-05-19 13:26:20 +0300

    add more examples to demo

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -7,22 +7,158 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  sections: RingSectionItem[] = [
-    {
-      percentage: 0.8,
-      color: 'red'
-    },
-    {
-      percentage: 0.1,
-      color: 'green'
-    },
-    {
-      percentage: 0.1,
-      color: 'blue'
-    }
-  ];
 
   centerColor = '#FFFFFF';
-  thickness = 20;
-  diameter = 700;
+
+  ring1$$ = {
+    sections: [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.1,
+        color: 'green'
+      },
+      {
+        percentage: 0.1,
+        color: 'blue'
+      }
+    ],
+    thickness: 8,
+    diameter: 50
+  };
+
+  ring2$$ = {
+    sections: [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.1,
+        color: 'green'
+      },
+      {
+        percentage: 0.1,
+        color: 'blue'
+      }
+    ],
+    thickness: 15,
+    diameter: 100
+  };
+
+  ring3$$ = {
+    sections: [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.1,
+        color: 'green'
+      },
+      {
+        percentage: 0.1,
+        color: 'blue'
+      }
+    ],
+    thickness: 30,
+    diameter: 200
+  };
+
+  ring4$$ = {
+    sections: [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.1,
+        color: 'green'
+      },
+      {
+        percentage: 0.1,
+        color: 'blue'
+      }
+    ],
+    thickness: 12,
+    diameter: 300
+  };
+
+  ring5$$ = {
+    sections: [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.1,
+        color: 'green'
+      },
+      {
+        percentage: 0.1,
+        color: 'blue'
+      }
+    ],
+    thickness: 25,
+    diameter: 50
+  };
+
+  ring6$$ = {
+    sections: [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.1,
+        color: 'green'
+      },
+      {
+        percentage: 0.1,
+        color: 'blue'
+      }
+    ],
+    thickness: 50,
+    diameter: 100
+  };
+
+  ring7$$ = {
+    sections: [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.1,
+        color: 'green'
+      },
+      {
+        percentage: 0.1,
+        color: 'blue'
+      }
+    ],
+    thickness: 100,
+    diameter: 200
+  };
+
+  ring8$$ = {
+    sections: [
+      {
+        percentage: 0.8,
+        color: 'red'
+      },
+      {
+        percentage: 0.1,
+        color: 'green'
+      },
+      {
+        percentage: 0.1,
+        color: 'blue'
+      }
+    ],
+    thickness: 150,
+    diameter: 300
+  };
 }
